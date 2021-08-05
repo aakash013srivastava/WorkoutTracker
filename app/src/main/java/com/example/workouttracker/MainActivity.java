4fe0b3b70079e9227cd719349ed42a1a271bd731
Aakash <aakash013srivastava@gmail.com>
2021-08-05 18:17:32 +0530

    Start/Stop Workout timings with breaks

---
+++ app/src/main/java/com/example/workouttracker/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.workouttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+Button startButton,breakButton,stopButton;
+String output = "";
+Boolean isBreak = false;
+String startTime = "",stopTime = "";
+public static final String TIME_STAT = "com.example.workouttracker.TIME_STAT";
+
+public static String timeCalc(){
+    SimpleDateFormat formatter= new SimpleDateFormat("dd-MM-yyyy 'at' HH:mm:ss z");
+    Date date = new Date(System.currentTimeMillis());
+    String curDate = (formatter.format(date));
+    return curDate;
+}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.startButton);
+        breakButton = findViewById(R.id.breakButton);
+        stopButton = findViewById(R.id.stopButton);
+
+        // Disabling Break and stop button initially
+        breakButton.setEnabled(false);
+        stopButton.setEnabled(false);
+
+        //Creating collection to store timestamps
+
+        // Start of workout start button event listener
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                breakButton.setEnabled(true);
+                stopButton.setEnabled(true);
+                startButton.setEnabled(false);
+
+                // Save workout start time
+                startTime = timeCalc();
+                //Add start time to output
+                output += startTime+"\n";
+                // To add break timestamps to output string
+                breakButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(!isBreak){
+                            String curDate = timeCalc();
+                            output += "Break Start At:"+curDate+"\n";
+                            Toast.makeText(getApplicationContext(), curDate, Toast.LENGTH_SHORT).show();
+                        }else{
+                            String curDate = timeCalc();
+                            output += "Break Over At:"+curDate+"\n";
+                            Toast.makeText(getApplicationContext(), curDate, Toast.LENGTH_SHORT).show();
+                        }
+                        isBreak = !isBreak;
+                    }
+                });
+                //
+                stopButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        breakButton.setEnabled(false);
+                        stopButton.setEnabled(false);
+                        startButton.setEnabled(true);
+
+                        // Save workout stop time
+                        stopTime = timeCalc();
+                        // Append stop time to output
+                        output += stopTime;
+                        Intent intent = new Intent(MainActivity.this,WorkoutTimingStat.class);
+                        intent.putExtra(TIME_STAT,output);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+    }
+}
